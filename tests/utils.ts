0e908b1fad7f303f37338c629c3e23d137ce385a
Lost4295 <turin-ylan@outlook.fr>
2025-04-02 16:25:20 +0200

    revert utils.ts

--- tests/utils.ts
+++ tests/utils.ts
@@ -4,7 +4,7 @@ import {CinemaSession} from "../src/db/models/CinemaSession"
 import {CinemaRoom} from "../src/db/models/CinemaRoom";
 
 export async function cleanDB() {
-    await AppDataSource.createQueryBuilder().from(Movie, "mov").delete().execute()
-    await AppDataSource.createQueryBuilder().from(CinemaRoom, "cr").delete().execute()
-    await AppDataSource.createQueryBuilder().from(CinemaSession, "cs").delete().execute()
+    await AppDataSource.createQueryBuilder().delete().from(Movie).execute()
+    await AppDataSource.createQueryBuilder().delete().from(CinemaRoom).execute()
+    await AppDataSource.createQueryBuilder().delete().from(CinemaSession).execute()
 }
